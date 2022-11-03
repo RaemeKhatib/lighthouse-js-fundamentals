974fb5f9710d3154c9c0dce5d2b02aae0eee39a3
Raeme Khatib <raeme.khatib@telus.com>
2022-11-03 17:24:11 -0600

    was able to push through the loop lighthouse

--- chorus-for.js
+++ chorus-for.js
@@ -6,3 +6,16 @@ console.log("Until the sun comes up!");
 for(x = 100; x<=200; x++){
   console.log(x);
 }
+
+
+
+for(let x = 100; x <= 200; x++) {
+  console.log(x);
+  if(x % 3 === 0){
+    console.log("Loopy");
+  } else if(x % 4 === 0){
+    console.log("Lighthouse");
+  } else if(x % 3 === 0 && x % 4 === 0){
+    console.log("Loopylighthouse");
+  }
+}
--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,10 +1,13 @@
-for(let x = 100; x <= 200; x++){
-  console.log(x);
-  if(x % 3 === 0){
+let start = 100;
+let end = 200;
+for (let i = start; i < end + 1; i++) {
+  if ((i % 3 === 0) && (i % 4 === 0)) {
+    console.log("LoopyLighthouse");
+  } else if (i % 3 === 0) {
     console.log("Loopy");
-  } if(x % 4 === 0){
+  } else if (i % 4 === 0){
     console.log("Lighthouse");
-  } if(x % 3 === 0 && x % 4 === 0){
-    console.log("Loopylighthouse");
+  } else {
+    console.log(i);
   }
 }
