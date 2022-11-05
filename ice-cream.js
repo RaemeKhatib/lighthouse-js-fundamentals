a397300a88fca2e2c6d9adb5bcbb32e7ce01a7f6
Raeme Khatib <raeme.khatib@telus.com>
2022-11-04 22:32:00 -0600

    added 2 more flavours

--- ice-cream.js
+++ ice-cream.js
@@ -1,7 +1,8 @@
 // Do not modify these first two lines
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
 
-// Your code below here...
 iceCreamFlavours.push("root beer");
 console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
